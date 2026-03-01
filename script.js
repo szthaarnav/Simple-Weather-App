701d5767e710d8947f3f20498929395b518c0bf6
szthaarnav <szthaarnav@gmail.com>
2026-03-01 19:02:21 +0545

    feat: country and city name fetched from api when user searches

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const ddlUnits = document.querySelector("#ddlUnits");
+let cityName, countryName;
 
 async function getGeoData() {
-  let search = "Nayabazar, Kirtipur"
-  const url = `https://nominatim.openstreetmap.org/search?q=${search}&format=jsonv2`;
+  let search = "Kathmandu"
+  const url = `https://nominatim.openstreetmap.org/search?q=${search}&format=jsonv2&addressdetails=1`;
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -13,10 +14,19 @@ async function getGeoData() {
     console.log(result);
     let lat = result[0].lat;
     let lon = result[0].lon;
+    LoadLocationData(result);
     getWeatherData(lat,lon);
   } catch (error) {
     console.error(error.message);
   }
+}
+
+function LoadLocationData(locationData){
+  let location =  locationData[0].address;
+  cityName = location.city;
+  countryName = location.country;
+  console.log(cityName, countryName);
+
 }
 
 async function getWeatherData(lat,lon) {
@@ -37,7 +47,7 @@ async function getWeatherData(lat,lon) {
   //wind_speed_unit = kmh(default) OR ms Or mph OR kn
   //precipitation_unit = mm(default) OR inch
 
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=weather_code,precipitation,temperature_2m,wind_speed_10m,relative_humidity_2m,apparent_temperature&wind_speed_unit=${windUnit}&temperature_unit=${tempUnit}&precipitation_unit=${precipUnit}`;
+  // const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=weather_code,precipitation,temperature_2m,wind_speed_10m,relative_humidity_2m,apparent_temperature&wind_speed_unit=${windUnit}&temperature_unit=${tempUnit}&precipitation_unit=${precipUnit}`;
   try {
     const response = await fetch(url);
     if (!response.ok) {
