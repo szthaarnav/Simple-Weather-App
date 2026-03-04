7b5dedd23dc3ceca3c75b3e899c5587e014b2d39
szthaarnav <szthaarnav@gmail.com>
2026-03-04 23:56:05 +0545

    test: weather codes each number initialized

--- script.js
+++ script.js
@@ -93,6 +93,17 @@ function LoadWeatherData(weather){
 
 }
 
+function getWeatherCode(code){
+  //Drizzle = 51, 53, 55, 56, 57
+  // Fog = 45, 48
+  // Overcast = 3
+  // Partly Cloudly = 1, 2
+  // Rain = 61, 63, 65, 66, 67, 80, 81, 82
+  // Snow = 71, 73, 75, 77, 85, 86
+  // Storm = 95, 96, 99
+  // Sunny = 0
+}
+
 ddlUnits.addEventListener("change", () => {
   getGeoData();
 });
